f59c751974913b90d14a996f3587f8e5ad7af41e
sevenseas773 <nana73smnm@gmail.com>
2024-08-11 16:24:17 +0900

    added FAQ

--- js/script.js
+++ js/script.js
@@ -82,5 +82,29 @@ $(".openbtn").click(function () {
     $(this).toggleClass('active');
     $('.nav').toggleClass('show');
 });
-
-  
+$('.slider2').slick({
+    arrows: true,//左右の矢印はなし
+    autoplay: false,//自動的に動き出すか。初期値はfalse。
+    autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+    speed: 0,//スライドのスピード。初期値は300。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+    pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+    cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+    slidesToShow: 1,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスライドで動かす要素数
+    responsive: [
+        {
+        breakpoint: 769,//モニターの横幅が769px以下の見せ方
+        settings: {
+            slidesToShow: 1,//スライドを画面に2枚見せる
+        }
+    },
+    {
+        breakpoint: 426,//モニターの横幅が426px以下の見せ方
+        settings: {
+            slidesToShow: 1,//スライドを画面に1.5枚見せる
+        }
+    }
+]
+});
